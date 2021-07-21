23c4251951be820e71f6c2aec655248225323f64
gabil-gurbanov-gadir <gabilgg@code.edu.az>
2021-07-22 00:51:55 +0400

    Yadimnan cixmisdi))

---
+++ HW_210719/Pustok/Models/Slider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        [StringLength(maximumLength:100)]
+        public string Title { get; set; }
+        [StringLength(maximumLength: 200)]
+        public string Subtitle { get; set; }
+        [Required]
+        [StringLength(maximumLength: 100)]
+        public string Image { get; set; }
+        [StringLength(maximumLength: 250)]
+        public string RedirectUrl { get; set; }
+        [Required]
+        public int Order { get; set; }
+  
+        [StringLength(maximumLength: 25)]
+        public string BtnText { get; set; }
+    }
+}
---
+++ HW_210719/Pustok/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Pustok.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.ViewModels
+{
+    public class LayoutViewModel
+    {
+        public Setting Setting { get; set; }
+
+        public List<Partner> Partners { get; set; }
+    }
+}
